ea085be58157b11dc029b074ec31b0f70599b161
Your Name <you@example.com>
2021-05-12 13:26:27 +0700

    M

--- src/utility/MD5.java
+++ src/utility/MD5.java
@@ -1,3 +1,26 @@
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class MD5 {
-    
+	public static String encypt(String text) {
+		MessageDigest md;
+				try {
+					md = MessageDigest.getInstance("MD5");
+					byte[] hashInBytes = md.digest(text.getBytes(StandardCharsets.UTF_8));
+
+			        StringBuilder sb = new StringBuilder();
+			        for (byte b : hashInBytes) {
+			            sb.append(String.format("%02x", b));
+			        }
+			        
+			        return sb.toString();
+				} catch (NoSuchAlgorithmException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+					return "";
+				}
+			
+		
+	}
 }
